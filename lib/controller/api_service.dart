99ef9b194d088591cb4d3400c5c3a79469c5e888
Muhammad Amin <mohamedamin.fcih@gmail.com>
2025-01-15 23:35:24 +0200

    modified api service

--- lib/controller/api_service.dart
+++ lib/controller/api_service.dart
@@ -1,3 +1,9 @@
+import 'package:dio/dio.dart';
+
 class ApiService {
   static const String url = 'https://jsonplaceholder.typicode.com/users';
+  final Dio _dio = Dio(BaseOptions(baseUrl: url));
+  Future<Response> updateUser(int id, Map<String, dynamic> data) async {
+    return await _dio.put('/users/$id', data: data);
+  }
 }
