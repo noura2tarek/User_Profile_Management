52d94dd4cd77a96de344dc0088551d2627d49ff4
emadmoenes <57167905+emadmoenes@users.noreply.github.com>
2025-01-18 20:24:03 +0200

    handle service

--- lib/controller/api_service.dart
+++ lib/controller/api_service.dart
@@ -1,22 +1,24 @@
 import 'package:dio/dio.dart';
 
+// End point url
+String url = 'https://jsonplaceholder.typicode.com/users';
+
 class ApiService {
-  final Dio _dio =
-      Dio(BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com/'));
+  final Dio _dio = Dio();
 
   Future<Response> fetchUsers() async {
-    return await _dio.get('/users');
+    return await _dio.get(url);
   }
 
   Future<Response> addUser(Map<String, dynamic> data) async {
-    return await _dio.post('/users', data: data);
+    return await _dio.post(url, data: data);
   }
 
   Future<Response> updateUser(int id, Map<String, dynamic> data) async {
-    return await _dio.put('/users/$id', data: data);
+    return await _dio.put('$url/$id', data: data);
   }
 
   Future<Response> deleteUser(int id) async {
-    return await _dio.delete('/users/$id');
+    return await _dio.delete('$url/$id');
   }
 }
--- lib/controller/user_service.dart
+++ lib/controller/user_service.dart
@@ -1,52 +1,117 @@
-import 'package:user_profile_management/controller/api_service.dart';
+import 'dart:developer';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:user_profile_management/model/user_model.dart';
 
-class UserController {
-  final ApiService _apiService = ApiService();
-  List<User> _users = [];
-  bool _loading = true;
+class ApiService {
+  final Dio _dio = Dio();
 
-  List<User> get users => _users;
-  bool get loading => _loading;
+  // End point url
+  String url = 'https://jsonplaceholder.typicode.com/users';
+  /*-------------- Add User Data Method ------------*/
 
-  Future<void> fetchUsers() async {
-    _loading = true;
-    try {
-      final response = await _apiService.fetchUsers();
-      _users =
-          (response.data as List).map((json) => User.fromJson(json)).toList();
-    } catch (e) {
-      print('Error fetching users: $e');
-    } finally {
-      _loading = false;
-    }
+  Future<Response> addUser(Map<String, dynamic> data) async {
+    return await _dio.post(url, data: data);
+  }
+  /*-------------- Update User Data Method ------------*/
+
+  Future<Response> updateUser(int id, Map<String, dynamic> data) async {
+    return await _dio.put('$url/$id', data: data);
+  }
+  /*-------------- Delete User Data Method ------------*/
+
+  Future<Response> deleteUser(int id) async {
+    return await _dio.delete('$url/$id');
   }
 
-  Future<void> addUser(User user) async {
+  /*-------------- Get User Data Method ------------*/
+  Future<List<User>> getUsersData() async {
+    List<User> users = [];
     try {
-      await _apiService.addUser(user.toJson());
-      _users.add(user);
-    } catch (e) {
-      print('Failed to add user: $e');
+      var response = await _dio.get(url);
+      var data = response.data;
+      if (response.statusCode == 200) {
+        // Convert json data to list of user model
+        data.forEach((user) {
+          users.add(User.fromJson(user));
+        });
+        // print(data);
+        // print(users.length);
+      }
+    } on DioException catch (e) {
+      // HANDLE ERRORS
+      if (e.type == DioExceptionType.badResponse) {
+        switch (e.response!.statusCode) {
+          case 404:
+            log('error: 404 - Not found');
+            // Cancels the currently visible toast, if any
+            Fluttertoast.cancel();
+            _showToast(
+              message: e.response?.data['message'],
+              color: _chooseToastColor(ToastStates.ERROR),
+            );
+            break;
+          case 401:
+            log('error: 401 - Unauthorized');
+            // Cancels the currently visible toast, if any
+            Fluttertoast.cancel();
+            _showToast(
+              message: e.response?.data['message'],
+              color: _chooseToastColor(ToastStates.ERROR),
+            );
+            break;
+          default:
+            log('error: ${e.response!.statusCode} - ${e.response?.data['message']}');
+            break;
+        }
+      } else if (e.type == DioExceptionType.connectionError) {
+        log('error: Please check your internet connection.');
+        // Cancels the currently visible toast, if any
+        Fluttertoast.cancel();
+        _showToast(
+          message: 'No internet connection, check your internet and try again',
+          color: _chooseToastColor(ToastStates.NOTIFY),
+        );
+      }
     }
+    return users;
   }
 
-  Future<void> updateUser(User user) async {
-    try {
-      await _apiService.updateUser(user.id, user.toJson());
-      final index = _users.indexWhere((u) => u.id == user.id);
-      _users[index] = user;
-    } catch (e) {
-      print('Failed to update user: $e');
-    }
+  /*---------------- Show Toast Method ---------------*/
+  Future<bool?> _showToast(
+      {required String message, Color color = Colors.red}) {
+    return Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: color,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
   }
 
-  Future<void> deleteUser(int id) async {
-    try {
-      await _apiService.deleteUser(id);
-      _users.removeWhere((user) => user.id == id);
-    } catch (e) {
-      print('Failed to delete user: $e');
+  // Choose toast color
+  Color _chooseToastColor(ToastStates state) {
+    Color color = Colors.red;
+    switch (state) {
+      case ToastStates.SUCCESS:
+        color = Colors.green;
+        break;
+      case ToastStates.WARNING:
+        color = Colors.yellow;
+        break;
+      case ToastStates.ERROR:
+        color = Colors.red;
+        break;
+      case ToastStates.NOTIFY:
+        color = Colors.grey;
+        break;
     }
+    return color;
   }
 }
+
+///////////////////// End of Class /////////////////////////
+enum ToastStates { SUCCESS, WARNING, NOTIFY, ERROR }
